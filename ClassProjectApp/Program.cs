c31456eddd331b4d52e5c47c9e7caec71d6c4707
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2025-09-07 21:58:20 -0400

    Fixed issue where CSS wasn't correct.

--- ClassProjectApp/Program.cs
+++ ClassProjectApp/Program.cs
@@ -14,12 +14,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
